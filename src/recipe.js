9953544e84dd04556a6a71957334c38e33742e92
Caroline Eubanks <cmeubanks@gmail.com>
2021-04-16 13:24:45 -0600

    completed Recipe Class test

--- src/recipe.js
+++ src/recipe.js
@@ -1,20 +1,21 @@
 class Recipe {
-  constructor(recipe, ingredientsData) {
-    this.name = recipe.name;
-    this.id = recipe.id; //in use
-    this.ingredients = recipe.ingredients; // in use
-    this.instructions = recipe.instructions;
-    this.tags = recipe.tags;
+  constructor(selectedRecipe, ingredientsData) {
+    // this.name = recipe.name;
+    // this.id = recipe.id; //in use
+    // this.ingredients = recipe.ingredients; // in use
+    // this.instructions = recipe.instructions;
+    // this.tags = recipe.tags;
+    this.recipe = selectedRecipe
     this.ingredientsData = ingredientsData; // in use
   }
 
   getCookingInstructions(type) {
-    return this.type
+    return this.recipe[type];
   }
 
   calculateCost() {
     let costCounter = 0;
-    this.ingredients.forEach(ingredient => {
+    this.recipe.ingredients.forEach(ingredient => {
       this.ingredientsData.find(specificIngredient => {
         if (specificIngredient.id === ingredient.id) {
           costCounter += (Number(specificIngredient.estimatedCostInCents) *
--- test/recipe-test.js
+++ test/recipe-test.js
@@ -115,14 +115,67 @@ describe('Recipe', () => {
     recipe = new Recipe(recipeData[0], ingredientsData);
   });
 
-  it('Should hold its own ingredient data', () => {
+  it('Should hold ingredient data', () => {
 
-    expect(recipe.ingredients).to.equal(recipeData[0].ingredients);
+    expect(recipe.ingredientsData).to.deep.equal([
+      {
+        "id": 20081,
+        "name": "wheat flour",
+        "estimatedCostInCents": 142
+      },
+      {
+        "id": 18372,
+        "name": "baking sode",
+        "estimatedCostInCents": 582
+      },
+      {
+        "id": 1009016,
+        "name": "apple cider",
+        "estimatedCostInCents": 472
+      },
+    ]);
   })
 
-  it('Should hold its own instruction data', () => {
+  it('Should hold a the recipe data of a chosen recipe', () => {
 
-    expect(recipe.instructions).to.equal(recipeData[0].instructions);
+    expect(recipe.recipe).to.deep.equal({
+      "name": "Loaded Chocolate Chip Pudding Cookie Cups",
+      "id": 595736,
+      "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+      "ingredients": [
+        {
+          "name": "all purpose flour",
+          "id": 20081,
+          "quantity": {
+            "amount": 1.5,
+            "unit": "c"
+          }
+        },
+        {
+          "name": "baking soda",
+          "id": 18372,
+          "quantity": {
+            "amount": 0.5,
+            "unit": "tsp"
+          }
+        },
+          ],
+      "instructions": [
+        {
+          "number": 1,
+          "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy."
+        },
+        {
+          "number": 2,
+          "instruction": "Add egg and vanilla and mix until combined."
+        },
+        ],
+      "tags": [
+        "antipasti",
+        "starter",
+        "snack"
+      ]
+    });
   })
 
   it('should be able to return the instructions of a specified recipe', () => {
