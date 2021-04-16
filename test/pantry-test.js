0df5cbf9cf4cc347ee8b1d705f7c8fa0d08fed7a
EllieAzaveda <elliemiller66@icloud.com>
2021-04-16 16:40:45 -0600

    Fix minor bug in showIngredients method

--- test/pantry-test.js
+++ test/pantry-test.js
@@ -7,20 +7,33 @@ const recipeData = require('../src/data/recipes.js');
 
 describe('Pantry', function() {
 
-      it.skip('should be an instance of Pantry class', function() {
+      it.only('should be an instance of Pantry class', function() {
         let pantry = new Pantry(ingredientsData);
 
         expect(pantry).to.be.an.instanceOf(Pantry);
       });
 
-      it.skip('should be able to show all ingredients', function() {
+      it.only('should be able to show all ingredients', function() {
+        let ingredientsData = [
+          {"id": 20081, "name": "all purpose flour", "quantity": {"amount": 1.5, "unit": "c"}},
+          {"id": 18372, "name": "baking soda", "quantity": {"amount": 0.5, "unit": "tsp"}},
+          {"id": 1123, "name": "egg", "quantity": {"amount": 1, "unit": "large"}},
+          {"id": 19335, "name": "granulated sugar", "quantity": {"amount": 0.5, "unit": "c"}},
+          {"id": 19206, "name": "instant vanilla pudding mix", "quantity": {"amount": 3, "unit": "Tbsp"}},
+          {"id": 19334, "name": "light brown sugar", "quantity": {"amount": 0.5, "unit": "c"}},
+          {"id": 2047, "name": "salt", "quantity": {"amount": 0.5, "unit": "tsp"}},
+          {"id": 1012047, "name": "sea salt", "quantity": {"amount": 24, "unit": "servings"}},
+          {"id": 10019903, "name": "semisweet chocolate chips", "quantity": {"amount": 2, "unit": "c"}},
+          {"id": 1145, "name": "unsalted butter", "quantity": {"amount": 0.5, "unit": "c"}},
+          {"id": 2050, "name": "vanilla extract", "quantity": {"amount": 0.5, "unit": "tsp"}}
+        ];
         let pantry = new Pantry(ingredientsData);
         let ingredients = pantry.viewAllIngredients();
 
-        expect(ingredients[0]).deep.equal('wheat flour');
+        expect(ingredients[0]).deep.equal('all purpose flour');
       });
 
-      it.skip('should be able to find a recipe', function() {
+      it.only('should be able to find a recipe', function() {
         let pantry = new Pantry(ingredientsData);
 
         expect(pantry.findIngredients('Loaded Chocolate Chip Pudding Cookie Cups')).to.deep.equal([
@@ -38,8 +51,8 @@ describe('Pantry', function() {
           ]);
       });
 
-      it('should be able to tell if we have all the ingredients we need for a recipe', function() {
-          let ingredientsDataEx = [
+      it.only('should be able to tell if we have all the ingredients we need for a recipe', function() {
+          let ingredientsData = [
             {"id": 20081, "name": "all purpose flour", "quantity": {"amount": 1.5, "unit": "c"}},
             {"id": 18372, "name": "baking soda", "quantity": {"amount": 0.5, "unit": "tsp"}},
             {"id": 1123, "name": "egg", "quantity": {"amount": 1, "unit": "large"}},
@@ -52,13 +65,13 @@ describe('Pantry', function() {
             {"id": 1145, "name": "unsalted butter", "quantity": {"amount": 0.5, "unit": "c"}},
             {"id": 2050, "name": "vanilla extract", "quantity": {"amount": 0.5, "unit": "tsp"}}
           ];
-          let pantry = new Pantry(ingredientsDataEx);
+          let pantry = new Pantry(ingredientsData);
 
           expect(pantry.canWeCook('Loaded Chocolate Chip Pudding Cookie Cups')).to.equal(this.hasIngredients = true);
       });
 
-      it.skip('should be able to remove all ingredients for recipe from pantry if we can cook a meal', function () {
-        let ingredientsDataEx = [
+      it.only('should be able to remove all ingredients for recipe from pantry if we can cook a meal', function () {
+        let ingredientsData = [
           {"id": 20081, "name": "all purpose flour", "quantity": {"amount": 1.5, "unit": "c"}},
           {"id": 18372, "name": "baking soda", "quantity": {"amount": 0.5, "unit": "tsp"}},
           {"id": 1123, "name": "egg", "quantity": {"amount": 1, "unit": "large"}},
@@ -71,14 +84,14 @@ describe('Pantry', function() {
           {"id": 1145, "name": "unsalted butter", "quantity": {"amount": 0.5, "unit": "c"}},
           {"id": 2050, "name": "vanilla extract", "quantity": {"amount": 0.5, "unit": "tsp"}}
         ];
-        let pantry = new Pantry(ingredientsDataEx);
+        let pantry = new Pantry(ingredientsData);
 
         expect(pantry.cookRecipe('Loaded Chocolate Chip Pudding Cookie Cups')).to.deep.equal([]);
       })
 
 
       it.only('should be able to tell what ingredients are missing for a recipe', function() {
-        let ingredientsDataEx = [
+        let ingredientsData = [
           {"id": 20081, "name": "all purpose flour", "quantity": {"amount": 1.5, "unit": "c"}},
           {"id": 18372, "name": "baking soda", "quantity": {"amount": 0.5, "unit": "tsp"}},
           {"id": 1123, "name": "egg", "quantity": {"amount": 1, "unit": "large"}},
@@ -90,7 +103,7 @@ describe('Pantry', function() {
           {"id": 10019903, "name": "semisweet chocolate chips", "quantity": {"amount": 2, "unit": "c"}},
           {"id": 1145, "name": "unsalted butter", "quantity": {"amount": 0.5, "unit": "c"}},
         ];
-          let pantry = new Pantry(ingredientsDataEx);
+          let pantry = new Pantry(ingredientsData);
 
           expect(pantry.showStillNeeded('Loaded Chocolate Chip Pudding Cookie Cups')).deep.equal(
             ["vanilla extract"]);
