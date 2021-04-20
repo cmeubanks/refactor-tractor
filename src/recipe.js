d277523198ee064d57f843f2dbfb8d9082285ada
Caroline Eubanks <cmeubanks@gmail.com>
2021-04-20 08:33:40 -0600

    reduce kind of working

--- src/recipe.js
+++ src/recipe.js
@@ -17,15 +17,21 @@ class Recipe {
     let newObj = this.recipe.ingredients.reduce((obj, ingred) => {
       // ingred.forEach()
       obj["id"] = ingred.id;
-      obj["quantity"] = ingred.quantity.reduce((obj, currQ) => {
-        obj["amount"] = currQ.amount, obj["unit"] = currQ.unit, obj["name"] = this.ingredientsData.filter(ingredient => ingred.id === ingredient.id)[0].name
-        return obj
-      }, {})
+      obj["quantity"] = {"amount": ingred.quantity.amount, "unit": ingred.quantity.unit, "name": this.ingredientsData.filter(ingredient => ingred.id === ingredient.id)[0].name}
       return obj
     },{});
-    newObj['instructions'] = this.recipe.instructions
+    // newObj['instructions'] = this.recipe.instructions
     return newObj
   }
+
+  // instantiateIngredients() {
+  //   return this.recipe.ingredients.map(recipeIngredient => {
+  //     const ingredientInfo = this.ingredientsData.find(ingredient => ingredient.id === recipeIngredient.id);
+  //     if (ingredientInfo) {
+  //       return new Ingredient(ingredientInfo, recipeIngredient);
+  //     }
+  //   });
+  // }
 
   calculateCost() {
     let costCounter = 0;
