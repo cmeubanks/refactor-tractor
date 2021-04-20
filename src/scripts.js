0d6ba954acdeb02b4a32947cf997b2744bdfd19f
Jim Charnesky <mijnesky@hotmail.com>
2021-04-20 17:53:21 -0600

    Delete line 14

--- src/scripts.js
+++ src/scripts.js
@@ -11,7 +11,7 @@ import Pantry from './pantry';
 import Recipe from './recipe';
 import User from './user';
 import Cookbook from './cookbook';
-import Ingredient from './ingredient';
+
 
 let favButton = document.querySelector('.view-favorites');
 let homeButton = document.querySelector('.home')
